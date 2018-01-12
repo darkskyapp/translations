d0e0c766e309f30a059f89c228ed71cea9022422
J. T. L <ironwallaby@gmail.com>
2018-01-12 13:57:30 -0500

    Convert to Translate class, cleanup

--- test.js
+++ test.js
@@ -1,146 +1,139 @@
 "use strict";
-const assert      = require("assert"),
-      fs          = require("fs"),
-      path        = require("path"),
-      template    = require("./lib/template"),
-      translation = require("./"),
-      util        = require("util");
+const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+const Translation = require("./lib/translation");
+const translations = require("./");
+const util = require("util");
 
-describe("translation", () => {
-  describe("template", () => {
-    const convert = template({
-            "foo": "bar",
-            "bar": "meeple $2",
-            "baz": (a, b) => "meeple " + b,
-            "quux": () => "glorple",
-          });
+describe("translations", () => {
+  describe("Translation", () => {
+    const test = new Translation({
+      "foo": "bar",
+      "bar": "meeple $2",
+      "baz": (a, b) => "meeple " + b,
+      "quux": () => "glorple",
+    });
 
     it("should return a number in string form", () => {
-      assert.strictEqual(convert(42), "42");
+      assert.strictEqual(test.translate(42), "42");
     });
 
     it("should throw an error given an unrecognized string", () => {
-      assert.throws(() => { convert("42"); });
+      assert.throws(() => { test.translate("42"); });
     });
 
     it("should apply an expected value conversion", () => {
-      assert.strictEqual(convert("foo"), "bar");
+      assert.strictEqual(test.translate("foo"), "bar");
     });
 
     it("should throw an error given a value expected to be a string", () => {
-      assert.throws(() => { convert("bar"); });
+      assert.throws(() => { test.translate("bar"); });
     });
 
     it("should throw an error given a value expected to be a function", () => {
-      assert.throws(() => { convert("baz"); });
+      assert.throws(() => { test.translate("baz"); });
     });
 
     it("should throw an error given an empty array", () => {
-      assert.throws(() => { convert([]); });
+      assert.throws(() => { test.translate([]); });
     });
 
     it("should apply a string template", () => {
-      assert.strictEqual(convert(["bar", 10, 20]), "meeple 20");
+      assert.strictEqual(test.translate(["bar", 10, 20]), "meeple 20");
     });
 
     it("should fail to apply a function with the wrong arity", () => {
-      assert.throws(() => { convert(["baz", 10, 20, 30]); });
+      assert.throws(() => { test.translate(["baz", 10, 20, 30]); });
     });
 
     it("should apply a function template", () => {
-      assert.strictEqual(convert(["baz", 10, 20]), "meeple 20");
+      assert.strictEqual(test.translate(["baz", 10, 20]), "meeple 20");
     });
 
     it("should recursively apply function templates", () => {
       /* Actually, a "meeple meeple bar" sounds like it'd be a pretty tasty
        * candy treat. */
       assert.strictEqual(
-        convert(["bar", 10, ["baz", 20, "foo"]]),
+        test.translate(["bar", 10, ["baz", 20, "foo"]]),
         "meeple meeple bar"
       );
     });
 
     it("should throw an error given undefined", () => {
-      assert.throws(() => { convert(undefined); });
+      assert.throws(() => { test.translate(undefined); });
     });
 
     it("should throw an error given null", () => {
-      assert.throws(() => { convert(null); });
+      assert.throws(() => { test.translate(null); });
     });
 
     it("should throw an error given an object", () => {
-      assert.throws(() => { convert({}); });
+      assert.throws(() => { test.translate({}); });
     });
 
     it("should apply a zero-argument function", () => {
-      assert.strictEqual(convert("quux"), "glorple");
+      assert.strictEqual(test.translate("quux"), "glorple");
     });
 
     it("should fail to apply a zero-argument function given arguments", () => {
-      assert.throws(() => { convert(["quux"]); });
+      assert.throws(() => { test.translate(["quux"]); });
     });
 
     it("should fail to apply a function template given a value", () => {
-      assert.throws(() => { convert("baz"); });
+      assert.throws(() => { test.translate("baz"); });
     });
 
     it("should provide context to functions", () => {
-      const convert = template({
-              "foo": function(a, b, c) {
-                assert.deepEqual(this, ["foo"]);
-                return "Moop.";
-              },
-              "bar": function() {
-                assert.deepEqual(this, ["foo", "bar"]);
-                return "Boop.";
-              },
-              "baz": function(a) {
-                assert.deepEqual(this, ["foo", "baz"]);
-                return "Soup.";
-              },
-              "quux": function() {
-                assert.deepEqual(this, ["foo", "baz", "quux"]);
-                return "Floop.";
-              },
-              "neem": function(a) {
-                assert.deepEqual(this, ["foo", "neem"]);
-                return "Bloop.";
-              },
-              "glorp": function(a) {
-                assert.deepEqual(this, ["foo", "neem", "glorp"]);
-                return "Rope?";
-              }
-            });
+      const test = new Translation({
+        "foo": function(a, b, c) {
+          assert.deepEqual(this, ["foo"]);
+          return "Moop.";
+        },
+        "bar": function() {
+          assert.deepEqual(this, ["foo", "bar"]);
+          return "Boop.";
+        },
+        "baz": function(a) {
+          assert.deepEqual(this, ["foo", "baz"]);
+          return "Soup.";
+        },
+        "quux": function() {
+          assert.deepEqual(this, ["foo", "baz", "quux"]);
+          return "Floop.";
+        },
+        "neem": function(a) {
+          assert.deepEqual(this, ["foo", "neem"]);
+          return "Bloop.";
+        },
+        "glorp": function(a) {
+          assert.deepEqual(this, ["foo", "neem", "glorp"]);
+          return "Rope?";
+        }
+      });
 
-      convert(["foo", "bar", ["baz", "quux"], ["neem", ["glorp", 42]]]);
+      test.translate(["foo", "bar", ["baz", "quux"], ["neem", ["glorp", 42]]]);
     });
   });
 
-  describe("language", () => {
+  describe("languages", () => {
     fs.readdirSync(path.join(__dirname, "test_cases")).forEach(lang => {
       if(lang.charAt(0) === ".") {
         return;
       }
 
-      const name      = path.basename(lang, ".json"),
-            translate = translation[name];
+      const name = path.basename(lang, ".json");
+      const translation = translations[name];
 
       describe(name, () => {
-        const cases = JSON.parse(
-                fs.readFileSync(
-                  path.join(__dirname, "test_cases", lang),
-                  "utf8"
-                )
-            );
-            
+        const cases = require("./test_cases/" + lang);
+
         Object.keys(cases).forEach(summary => {
           const source = cases[summary];
 
           it(
             util.format("should translate %j to \"%s\"", source, summary),
-            () => {
-              assert.strictEqual(translate(source), summary);
-            }
+            () => assert.strictEqual(translation.translate(source), summary),
           );
         });
       });
