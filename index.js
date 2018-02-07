d006c1fde9463b7c2f566356d917f623a41d3776
Jay LaPorte <ironwallaby@gmail.com>
2018-02-07 16:23:24 -0500

    2.0 (#165)
    * Rename nb to no (until nynorsk is contributed, anyway)
    
    * Convert to Translate class, cleanup
    
    * Clean up tests
    
    * Clarify weekly summaries in en, fr #158

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 const fs = require("fs");
 const path = require("path");
+const Translation = require("./lib/translation");
 
 for(const pathname of fs.readdirSync(path.join(__dirname, "lib/lang"))) {
   const match = /^([^\.].*)\.js$/.exec(pathname);
 
   if(match) {
-    exports[match[1]] = require("./lib/lang/" + pathname);
+    exports[match[1]] = new Translation(Object.freeze(require("./lib/lang/" + pathname)));
   }
 }
