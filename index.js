dce57e50c992ea8754a49e1ac43531dabc64a827
J. T. L <ironwallaby@gmail.com>
2018-03-23 11:12:28 -0400

    Fix linter errors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const path = require("path");
 const Translation = require("./lib/translation");
 
 for(const pathname of fs.readdirSync(path.join(__dirname, "lib/lang"))) {
-  const match = /^([^\.].*)\.js$/.exec(pathname);
+  const match = /^([^.].*)\.js$/.exec(pathname);
 
   if(match) {
-    exports[match[1]] = new Translation(Object.freeze(require("./lib/lang/" + pathname)));
+    const template = require("./lib/lang/" + pathname);
+    Object.freeze(template);
+    exports[match[1]] = new Translation(template);
   }
 }
